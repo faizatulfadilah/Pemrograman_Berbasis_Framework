b2de9f2017adc23c75828b67f2ee49b6cc6299fc
faizatulfadilah <dhela.faizah@gmail.com>
2021-03-02 16:26:01 +0700

    upload pbf

---
+++ modul_2/week2/destructuring.js
@@ -0,0 +1,7 @@
+let polStudent = ({name, polytechnic}) => {
+    alert(`Hai ${name} from ${polytechnic}`);
+};
+polStudent({
+    name: 'Ronaldo',
+    polytechnic: 'Politeknik Negeri Malang'
+});
---
+++ modul_2/week2/template.js
@@ -0,0 +1,7 @@
+let fname = 'Christian';
+let lname = 'Ronaldo';
+let age = prompt("Masukkan Umur Christian Ronaldo");
+
+// Memakai Template Strings
+let result = '${fname} ${lname} is ${age}  years old';
+alert(result);
