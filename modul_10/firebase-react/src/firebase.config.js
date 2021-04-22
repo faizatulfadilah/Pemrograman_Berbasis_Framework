81ee7eb74f310680bc03791ddc75f284aec8d34b
faizatulfadilah <dhela.faizah@gmail.com>
2021-04-22 18:31:39 +0700

    Upload modul 10

---
+++ modul_10/firebase-react/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyBCwB1Vc-bqA2RklPUpfz_sd5Gbg8ZN3PM",
+    authDomain: "modul10-9340b.firebaseapp.com",
+    projectId: "modul10-9340b",
+    storageBucket: "modul10-9340b.appspot.com",
+    messagingSenderId: "177771795997",
+    appId: "1:177771795997:web:82b354e9d0c43c1de758f4",
+    measurementId: "G-33V39EW5R3"
+}
