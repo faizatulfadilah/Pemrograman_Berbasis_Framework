e57a8a04ba6a2c7374c4830c609bd4cd939b0b81
faizatulfadilah <dhela.faizah@gmail.com>
2021-05-03 18:03:31 +0700

    upload modul 12

---
+++ modul_12/laravelapp/resources/views/profil.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>BIODATA DIRI</title>
+<body>
+    <h1 align="center">BIODATA DIRI</h1>
+    <table width="745" border="1" cellspacing="0" cellpadding="5" align="center">
+    <tr align="center" bgcolor="#1EACE7">
+    <td width="174">DATA DIRI
+    <td width="353">KETERANGAN
+    </tr>
+    <tr>
+        <td>Nama</td>
+        <td>Faizatul Fadilah</td>
+    </tr>
+    <tr>
+        <td>Jurusan</td>
+        <td>Teknologi Informasi</td>
+    </tr>
+    <tr>
+        <td>Program Studi</td>
+        <td>Teknik Informatika</td>
+    </tr>
+    <tr>
+        <td>Alamat</td>
+        <td>Jl. Urip Sumoharjo Gang Pusaka Pabian Sumenep</td>
+    </tr>
+    <tr>
+        <td>Tempat Lahir</td>
+        <td>Sumenep</td>
+    </tr>
+    <tr>
+        <td>Tanggal Lahir</td>
+        <td>22 Januari 2000</td>
+    </tr>
+    <tr>
+        <td>Status Mahasiswa</td>
+        <td>Aktif</td>
+    </tr>
+    </table>
+</body>
+</html>
